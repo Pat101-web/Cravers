b46c5c2e9a677b5a2e48a42f4abeea4530ba8fe0
Patrick Etim <etimpatrick34@gmail.com>
2025-10-24 16:16:35 +0100

    update

--- public/transaction.php
+++ public/transaction.php
@@ -1,7 +1,7 @@
 <?php
 // public/transaction.php
-session_start();
-require_once __DIR__ . '/../config/db.php';
+//session_start();
+//require_once __DIR__ . '/../config/db.php';
 
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
