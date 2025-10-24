10ea4bdcb90056962b23b9b09c551db6c30b22c8
Patrick Etim <etimpatrick34@gmail.com>
2025-10-24 16:04:41 +0100

    update

--- public/signup.php
+++ public/signup.php
@@ -1,7 +1,8 @@
 
 <?php
+
 session_start();
-require_once __DIR__ . '/../config/db.php';
+//require_once __DIR__ . '/../config/db.php';
 
 if (isset($_SESSION['user_id'])) {
     header('Location: profile.php');
